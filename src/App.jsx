f7ab55c9b53618c75cc3b2551849c39a0e01da72
rishi190104 <rs9911777@gmail.com>
2024-11-15 16:20:19 +0530

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='w-full max-w-screen-xl mx-auto bg-red-300'>Hello world</h1>
+    </>
+  )
+}
+
+export default App
